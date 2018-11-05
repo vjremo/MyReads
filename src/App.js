dc6ca2a7362164215a2aef606ab05cc257485fb6
Vijay G <vjremo@gmail.com>
2018-11-04 22:12:47 -0500

    feat : separted book list and search page components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ListBooks from './ListBooks'
+import SearchBook from './SearchBook'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -16,51 +17,30 @@ class BooksApp extends React.Component {
     books: []
   }
   componentDidMount() {
+
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
     
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-  }
+  
   render() {
 
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {
-                  /*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" 
-                value = {this.state.query} 
-                placeholder="Search by title or author"
-                onChange={(event) => this.updateQuery(event.target.value)}/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchBook
+          books = {this.state.books} 
+          query = {this.state.query}/>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
                       <h1>MyReads</h1>
                   </div>
             <ListBooks 
-          books = {this.state.books} />
+          books = {this.state.books}
+          query = {this.state.query} />
             <div className="open-search">
               <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
             </div>
