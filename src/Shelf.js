6e9edc6281f92a652f57f0def39992df72000d9c
Vijay G <vjremo@gmail.com>
2018-11-10 11:38:37 -0500

    feat : converted class to function component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class Shelf extends Component{
+function Shelf(props){
+    const { books, name, updateBookShelf } = props
 
-    render(){
-        const { books, name, updateBookShelf } = this.props
-        
-        return(
-
-            <div className="bookshelf">
+    return (
+        <div className="bookshelf">
             {/*Assign shelf name, the list of books in that shelf, onChange function from props */}
-					  <h2 className="bookshelf-title">{name}</h2>
-					  <div className="bookshelf-books">
-						<ol className="books-grid">
-                        {
-                            books.map((book) => 
-                            <Book book={book} key={book.id} updateBookShelf={updateBookShelf}/>)
-                        }
-						</ol>
-					  </div>
-					</div>
-            )
-    }
+			<h2 className="bookshelf-title">{name}</h2>
+				<div className="bookshelf-books">
+                    <ol className="books-grid">
+                    {
+                        books.map((book) => 
+                        <Book book={book} key={book.id} updateBookShelf={updateBookShelf}/>)
+                    }
+                    </ol>
+			  </div>
+		</div>
+
+    )
 }
 
 export default Shelf
